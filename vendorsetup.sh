f11fb364c40a25e9c8a153e3b56352575a72f57b
brurmonemt <brurmonemt@protonmail.com>
2025-08-31 11:25:30 -0500

    fox_genevn.mk -> vendorsetup.sh
    Because this previous idea was just plain stupid

---
+++ vendorsetup.sh
@@ -0,0 +1,19 @@
+FDEVICE="genevn"
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+  # Main
+  export FOX_VANILLA_BUILD=1
+  
+  # GUI settings
+  export OF_STATUS_INDENT_LEFT=48
+  export OF_STATUS_INDENT_RIGHT=48
+
+  if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+    export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+    export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+    export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+    export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+  fi
+
+	add_lunch_combo twrp_"$FDEVICE"-eng
+	add_lunch_combo twrp_"$FDEVICE"-userdebug
+fi
